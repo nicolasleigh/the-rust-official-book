75aac23c6447a5629d47a949195e14ee43e2dbdd
Nicolas Leigh <linze201@gmail.com>
2025-01-06 12:59:23 +0800

    ch11: Writing Automated Tests

---
+++ ch11-src/lib.rs
@@ -0,0 +1,91 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {name}!")
+}
+
+pub struct Guess {
+    value: i32,
+}
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {value}.");
+        }
+
+        Guess { value }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn exploration() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{result}`"
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    // Using Result<T, E> in Tests
+    #[test]
+    fn it_works() -> Result<(), String> {
+        let result = add(2, 2);
+        if result == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
+    // Ignoring Some Tests Unless Specifically Requested
+    // If we want to run only the ignored tests, we can use `cargo test -- --ignored`
+    // If you want to run all tests whether they’re ignored or not, you can run `cargo test -- --include-ignored`.
+    #[test]
+    #[ignore]
+    fn expensive_test() {
+        // code that takes an hour to run
+    }
+}
